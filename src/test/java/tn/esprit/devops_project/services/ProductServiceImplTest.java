11e1845a61c2f42ee30138a09f98c933cff2e121
Talbi Jihed <talbijihed302@gmail.com>
2023-11-08 21:00:54 +0100

    feat:sonarqube correction code smells

--- src/test/java/tn/esprit/devops_project/services/ProductServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/ProductServiceImplTest.java
@@ -338,7 +338,7 @@ class ProductServiceImplTest {
     }
     @Test
     void deleteExistingProductWithAutowired() {
-        Product product = new Product(100L,"Product 1", 100.0f, 10, ProductCategory.ELECTRONICS);
+        Product product = new Product("Product 1", 100.0f, 10, ProductCategory.ELECTRONICS);
         productService.addProduct(product,1L);
         productService.deleteProduct(product.getIdProduct());
         assertFalse(productRepository.existsById(product.getIdProduct()));
